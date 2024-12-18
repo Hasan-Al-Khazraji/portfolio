dda4b4c6ac922a7f792f97f1d4976e2c5863ec40
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-12-18 11:43:50 -0500

    Enhance QuickLinks component with hover effects for improved interactivity

--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -5,13 +5,13 @@ import EmailIcon from '@mui/icons-material/Email';
 export default function QuickLinks() {
     return (
         <div className="flex">
-            <a href="https://github.com/Hasan-Al-Khazraji" target='_blank'>
+            <a href="https://github.com/Hasan-Al-Khazraji" target='_blank' className='hover:scale-110 transition-all'>
                 <GitHubIcon className='text-white scale-125 m-2 ml-1 hover:cursor-pointer'/>
             </a>
-            <a href="https://www.linkedin.com/in/hasan-al-khazraji/" target='_blank'>
+            <a href="https://www.linkedin.com/in/hasan-al-khazraji/" target='_blank' className='hover:scale-110'>
                 <LinkedInIcon className='text-white scale-125 m-2 hover:cursor-pointer'/>
             </a>
-            <a href="mailto:hasanwatheq2014@gmail.com" target='_blank'>
+            <a href="mailto:hasanwatheq2014@gmail.com" target='_blank' className='hover:scale-110'>
                 <EmailIcon className='text-white scale-125 m-2 hover:cursor-pointer'/>
             </a>
         </div>
