a6cd0ad13b271af4be0420076034ce4a38ec6aae
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-12-18 11:55:57 -0500

    Update About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import Projects from './Projects';
 export default function About() {
     return (
         <div>
-            <h2 className="text-gray-500 text-xl mb-8">Hi! I'm Hasan, a 20 y/o Computer Engineering Student. I love building things and solving problems.</h2>
+            <h2 className="text-gray-500 text-xl mb-8">Hi! I'm Hasan, a Computer Engineering Student. I love building things and solving problems.</h2>
             <Experience />
             <Projects />
         </div>
