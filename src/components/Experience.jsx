1d71e63680062355a75cfabad4429f3d7c26d24c
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-12-18 11:34:52 -0500

    Enhance Experience component header with decorative asterisk

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,7 @@
 export default function Experience() {
     return (
         <div>
-            <h1 className="text-white text-2xl mb-3">Experience</h1>
+            <h1 className="text-white text-2xl mb-3"><a className="text-orange-500 text-2xl">*</a> Experience</h1>
             <ol className="relative border-l-2 border-gray-900">
                 <li className="mb-10 ms-4">
                     <div className="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -start-1.5 border border-gray-900"></div>
