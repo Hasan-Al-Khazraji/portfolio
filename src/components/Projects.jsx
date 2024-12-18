6294527bbe1213e76de6af3531363c1205076b14
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-12-18 11:41:52 -0500

    Enhance Projects component link with hover effects and improved icon animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,8 +18,11 @@ export default function Projects() {
                 );
                 })}
             </ul>
-            <div className="w-fit">
-                <a href="https://github.com/Hasan-Al-Khazraji" target="_blank"><h3 className="text-orange-500 text-sm">View More Projects <ArrowOutwardIcon></ArrowOutwardIcon></h3></a>
+            <div className="w-fit flex text-orange-500 text-sm group">
+                <a href="https://github.com/Hasan-Al-Khazraji" target="_blank"><h3 className="group-hover:underline">View More Projects </h3></a>
+                <div className="-translate-y-0.5 transition-all group-hover:translate-x-1 group-hover:-translate-y-1">
+                    <ArrowOutwardIcon></ArrowOutwardIcon>
+                </div>
             </div>
         </div>
     );
