d3c6e1f158729d435c52855512b37607c42a183b
Hasan Al-Khazraji <hasanwatheq2014@gmail.com>
2025-05-23 04:14:02 -0400

    Move self image to right side

--- src/components/MastHead.jsx
+++ src/components/MastHead.jsx
@@ -3,11 +3,13 @@ import QuickLinks from "./QuickLinks";
 
 export default function MastHead() {
     return (
-        <div>
-            <img src="me.jpg" alt="Hasan" className="rounded-full h-16 w-16 object-cover mb-2"/>
-            <h1 className="text-white text-2xl">Hasan Al-Khazraji</h1>
-            <h2 className="text-gray-500 mb-2 text-xl">Software Engineering Intern</h2>
-            <QuickLinks />
+        <div className="flex items-center justify-between">
+            <div>
+                <h1 className="text-white text-2xl">Hasan Al-Khazraji</h1>
+                <h2 className="text-gray-500 mb-2 text-xl">Software Engineering Intern</h2>
+                <QuickLinks />
+            </div>
+            <img src="me.jpg" alt="Hasan" className="rounded-full h-16 w-16 sm:h-24 sm:w-24 object-cover mb-2"/>
         </div>
     );
 }
